481b3ac8fd1d6e2a4f5c3609bb96f6e056d20dde
Thomas Axelsson <thomasa88@gmail.com>
2026-04-04 08:38:15 +0200

    tmr-client: Drop unused OauthStore
    It was never used and did not store enough information.

--- tmr-client/src/cred_store.rs
+++ tmr-client/src/cred_store.rs
@@ -6,8 +6,7 @@ use tracing::debug;
 
 use async_trait::async_trait;
 use etcetera::AppStrategy;
-use rmcp::transport::{AuthError, StoredCredentials, auth::OAuthTokenResponse};
-use serde::{Deserialize, Serialize};
+use rmcp::transport::{AuthError, StoredCredentials};
 
 pub struct CredStore {
     filename: PathBuf,
@@ -44,38 +43,5 @@ impl rmcp::transport::CredentialStore for CredStore {
         std::fs::remove_file(&self.filename).ok();
         debug!("Cleared credentials at {:?}", self.filename);
         Ok(())
-    }
-}
-
-pub struct OauthStore {
-    filename: PathBuf,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct OauthCredentials {
-    client_id: String,
-    token_response: OAuthTokenResponse,
-}
-
-impl OauthStore {
-    pub fn new(dirs: &etcetera::app_strategy::Xdg) -> Self {
-        let filename = dirs.config_dir().join("oauth_credentials.json");
-        std::fs::create_dir_all(filename.parent().unwrap()).unwrap();
-        Self { filename }
-    }
-
-    pub fn save(&self, client_id: &str, credentials: &OAuthTokenResponse) {
-        let creds = OauthCredentials {
-            client_id: client_id.to_string(),
-            token_response: credentials.clone(),
-        };
-        let file = std::fs::File::create(&self.filename).unwrap();
-        serde_json::to_writer_pretty(file, &creds).unwrap();
-    }
-
-    pub fn load(&self) -> Option<(String, OAuthTokenResponse)> {
-        let file = std::fs::File::open(&self.filename).ok()?;
-        let creds: OauthCredentials = serde_json::from_reader(file).ok()?;
-        Some((creds.client_id, creds.token_response))
     }
 }
